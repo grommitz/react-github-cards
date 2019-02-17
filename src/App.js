9c222156b38dc717d19ca6793d89cf8dddc5bd87
grommitz <martincharlesworth@gmail.com>
2019-02-17 00:40:27 +0000

    basic cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -24,5 +25,34 @@ class App extends Component {
     );
   }
 }
+
+const Card = (props) => {
+  return (
+      <div style={{margin: '1em'}}>
+          <img width="75" style={{display:'inline-block', marginLeft:10}} 
+              src={props.avatar_url} />
+          <div className="info">
+              <div style={{fontSize: '1.25em', fontWeight:'bold'}}>{props.name}</div>
+              <div>{props.company}</div>
+          </div>
+      </div>      
+  );
+};
+
+const CardList = (props) => {
+  return (
+      <div>
+          <Card name="Martin Charlesworth" 
+              avatar_url="https://avatars0.githubusercontent.com/u/2547666?v=4"
+              company="CSC"/>
+          <Card name="Diabolic Developer" 
+              avatar_url="https://avatars0.githubusercontent.com/u/254123?v=4"
+              company="XXX"/>
+          <Card name="Terry" 
+              avatar_url="https://avatars2.githubusercontent.com/u/32106?v=4"
+              company="Company"/>
+      </div>
+  )
+};
 
 export default App;
