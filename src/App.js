5998fc4119f131897e6612a3c6d2a684210ef026
grommitz <martincharlesworth@gmail.com>
2019-02-17 01:44:08 +0000

    styles

--- src/App.js
+++ src/App.js
@@ -58,9 +58,9 @@ class Form extends React.Component {
 const Card = (props) => {
   return (
       <div style={{margin: '1em'}}>
-          <img width="75" style={{display:'inline-block', marginLeft:10}} 
+          <img width="75" 
               src={props.avatar_url} />
-          <div className="info">
+          <div style={{display:'inline-block', marginLeft:10}}>
               <div style={{fontSize: '1.25em', fontWeight:'bold'}}>{props.name}</div>
               <div>{props.company}</div>
           </div>
